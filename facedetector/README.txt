b7bef357c29139c32c44fc57ad61446bb2ad67d1
hogelog <konbu.komuro@gmail.com>
2011-05-11 01:51:34 +0900

    顔検出のコードを追加。

---
+++ facedetector/README.txt
@@ -0,0 +1,12 @@
+libの中のインストーラからそれぞれインストール。
+
+ - python -> C:\Python27
+ - OpenCV -> C:\OpenCV2.2
+ - numpyはpythonの後に入れれば自動的に入る。
+
+C:\OpenCV2.2\Python2.7\Lib\site-packages
+の中身を
+C:\Python27\Lib\site-packages
+にコピー。
+
+これで動くようになる（はず）。
---
+++ facedetector/eye.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# vim: fileencoding=utf-8:
+
+# *参考 
+#  * リファレンス
+# http://opencv.jp/opencv-2.2/py/objdetect_cascade_classification.html#haar-feature-based-cascade-classifier-for-object-detection
+#  * 解説ページ（C言語）
+# http://www.aianet.ne.jp/~asada/prog_doc/opencv/opencv_obj_det_img.htm
+
+import cv
+
+storage = cv.CreateMemStorage()
+
+# 「目検出」のための教師データの読み込み
+hc = cv.Load("C:/OpenCV2.2/data/haarcascades/haarcascade_eye.xml")
+
+
+# 画像の読み込み
+img = cv.LoadImageM("face.jpg")
+
+# 顔認識（速度のため適当にパラメータ）
+#faces = cv.HaarDetectObjects(img, hc, storage)
+faces = cv.HaarDetectObjects(img, hc, storage, 1.1, 100, 0, (100, 100))
+
+# (R, G, B)
+color = (255, 255, 255)
+
+# 検出したパーツそれぞれの領域を
+# 四角で囲む
+for (x, y, w, h), n in faces:
+  p1 = (x, y)
+  p2 = (x + w, y + h)
+  cv.Rectangle(img, p1, p2, color)
+
+# 四角を描いた画像を保存
+cv.SaveImage("face_detected.jpg", img)
